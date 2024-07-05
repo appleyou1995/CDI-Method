fe4ea57f774e7c58201c29d67926316edf0e0b07
Iris Yu <jouping.yu@gmail.com>
2024-07-05 22:36:35 +0800

    Simplified the Physical_Density function by removing unnecessary variables and directly using month names as column names

--- 03  條件密度積分方法（CDI method）/Physical_Density.m
+++ 03  條件密度積分方法（CDI method）/Physical_Density.m
@@ -1,19 +1,12 @@
 function AllR_PD = Physical_Density(Smooth_ALLR, Smooth_AllR_RND, All_g_hat)
-    
+
     months = Smooth_ALLR.Properties.VariableNames;
-    T = length(months);
     AllR_PD = table();
 
-    for t = 1:T
-
-        current_month_RND = Smooth_AllR_RND{1, months{t}};
-        current_month_g_hat = All_g_hat{1, months{t}};
-                
-        current_month_PD = current_month_RND .* current_month_g_hat;
-
-        columnName = num2str(months{t});
-        AllR_PD.(columnName) = current_month_PD;
-
+    for t = 1:length(months)
+        
+        current_month_PD = Smooth_AllR_RND{1, months{t}} .* All_g_hat{1, months{t}};
+        AllR_PD.(months{t}) = current_month_PD;
+        
     end
-    
 end
