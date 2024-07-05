15b34e0875e433555b9b13fb8d210f92673840de
Iris Yu <jouping.yu@gmail.com>
2024-07-05 20:47:59 +0800

    Set options to disable iterative display

--- 03  條件密度積分方法（CDI method）/gmm_theta_estimation.m
+++ 03  條件密度積分方法（CDI method）/gmm_theta_estimation.m
@@ -7,7 +7,7 @@ function theta_hat = gmm_theta_estimation(months, Smooth_ALLR, Smooth_AllR_RND, 
     theta0 = rand(1, b);
     
     % Optimization options
-    options = optimoptions('fminunc', 'Display', 'iter', 'Algorithm', 'quasi-newton');
+    options = optimoptions('fminunc', 'Display', 'off', 'Algorithm', 'quasi-newton');
     
     % Minimize the objective function
     theta_hat = fminunc(@(theta) gmm_objective_function(theta, months, Smooth_ALLR, Smooth_AllR_RND, b), theta0, options);
