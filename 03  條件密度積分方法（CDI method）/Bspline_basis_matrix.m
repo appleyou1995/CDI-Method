fd8f3c3520c415d77fe8fc9ec599154083beb743
Iris Yu <jouping.yu@gmail.com>
2024-07-05 21:39:08 +0800

    Consolidate local functions into main function file for better organization and reduce the number of m-files

--- 03  條件密度積分方法（CDI method）/Bspline_basis_matrix.m
+++ 03  條件密度積分方法（CDI method）/Bspline_basis_matrix.m
@@ -11,3 +11,16 @@ function M = Bspline_basis_matrix(k)
     end
     
 end
+
+
+%% Elements in B-spline basis matrix
+
+function mij = Bspline_basis_matrix_mij(i, j, k)
+
+    sum_term = 0;
+    for s = j:k-1
+        sum_term = sum_term + (-1)^(s - j) * nchoosek(k, s - j) * (k - s - 1)^(k - 1 - i);
+    end
+    mij = nchoosek(k - 1, k - 1 - i) * sum_term;
+
+end
--- 03  條件密度積分方法（CDI method）/gmm_theta_estimation.m
+++ 03  條件密度積分方法（CDI method）/gmm_theta_estimation.m
@@ -1,5 +1,7 @@
-function theta_hat = gmm_theta_estimation(Smooth_ALLR, Smooth_AllR_RND, b)
-    
+%% Main Function: GMM Theta Estimation
+
+function theta_hat = GMM_theta_estimation(Smooth_ALLR, Smooth_AllR_RND, b)
+
     % Set the seed for the random number generator
     rng(0);
 
@@ -12,4 +14,53 @@ function theta_hat = gmm_theta_estimation(Smooth_ALLR, Smooth_AllR_RND, b)
     % Minimize the objective function
     theta_hat = fminunc(@(theta) gmm_objective_function(theta, Smooth_ALLR, Smooth_AllR_RND, b), theta0, options);
 
+end
+
+
+%% Local Function: GMM Objective Function
+
+function J = gmm_objective_function(theta, Smooth_ALLR, Smooth_AllR_RND, b)
+
+    g = gmm_moment_conditions(theta, Smooth_ALLR, Smooth_AllR_RND, b);
+
+    % Use a GMM type optimization with only the first stage optimization
+    W = eye(b);
+
+    % Objective function
+    J = g' * W * g;
+end
+
+
+%% Local Function: GMM Moment Conditions
+
+function g = gmm_moment_conditions(theta, Smooth_ALLR, Smooth_AllR_RND, b)
+
+    months = Smooth_ALLR.Properties.VariableNames;
+    T = length(months);
+    m = b;
+    g = zeros(m, 1);
+    B = Bspline_basis_functions(b);
+
+    for j = 1:m
+        moment_sum = 0;
+
+        for t = 1:T
+            current_month_y = Smooth_ALLR{1, months{t}};
+            current_month_RND = Smooth_AllR_RND{1, months{t}};
+
+            g_theta = 0;
+
+            for i = 1:b        
+                B_function = matlabFunction(B{i});
+                B_values = B_function(current_month_y);
+                
+                integral = trapz(current_month_y, B_values .* current_month_RND);            
+                g_theta = g_theta + theta(i) * integral;
+            end
+
+            moment_sum = moment_sum + g_theta ^ j;
+        end
+
+        g(j) = moment_sum / T - 1 / (j + 1);
+    end
 end
