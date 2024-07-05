aab6ca201f87f4616f59a8f71d0f02eeba6cf5f1
Iris Yu <jouping.yu@gmail.com>
2024-07-05 21:59:10 +0800

    Change the code for g function estimation from a single month to all months

--- 03  條件密度積分方法（CDI method）/g_function_estimation.m
+++ 03  條件密度積分方法（CDI method）/g_function_estimation.m
@@ -1,12 +1,25 @@
-function g_hat = g_function_estimation(theta_hat, current_month_y, b, B)
+function All_g_hat = g_function_estimation(theta_hat, Smooth_ALLR, b)
     
-    B_matrix = zeros(length(current_month_y), b);
-    
-    for i = 1:b
-        B_function = matlabFunction(B{i});
-        B_matrix(:, i) = B_function(current_month_y);
+    months = Smooth_ALLR.Properties.VariableNames;
+    T = length(months);
+    B = Bspline_basis_functions(b);
+    All_g_hat = table();
+
+    for t = 1:T
+
+        current_month_y = Smooth_ALLR{1, months{t}};
+        B_matrix = zeros(length(current_month_y), b);
+        
+        for i = 1:b
+            B_function = matlabFunction(B{i});
+            B_matrix(:, i) = B_function(current_month_y);
+        end
+        
+        g_hat = (B_matrix * theta_hat(:)).';
+
+        columnName = num2str(months{t});
+        All_g_hat.(columnName) = g_hat;
+
     end
-    
-    g_hat = (B_matrix * theta_hat(:)).';
 
 end
