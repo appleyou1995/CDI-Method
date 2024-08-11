ae354406e74d992e8ce45ce13103d58403ca74f4
Iris Yu <jouping.yu@gmail.com>
2024-08-11 16:28:45 +0800

    Adjust the upper limit of points in the process of estimating theta to the realized  return

--- 03  條件密度積分方法（CDI method）/GMM_theta_estimation.m
+++ 03  條件密度積分方法（CDI method）/GMM_theta_estimation.m
@@ -1,6 +1,6 @@
 %% Main Function: GMM Theta Estimation
 
-function theta_hat = GMM_theta_estimation(Smooth_AllR, Smooth_AllR_RND, b, min_knot, max_knot)
+function theta_hat = GMM_theta_estimation(Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot)
 
     % Set the seed for the random number generator
     rng(0);
@@ -12,16 +12,16 @@ function theta_hat = GMM_theta_estimation(Smooth_AllR, Smooth_AllR_RND, b, min_k
     options = optimoptions('fminunc', 'Display', 'iter', 'Algorithm', 'quasi-newton');
     
     % Minimize the objective function
-    theta_hat = fminunc(@(theta) GMM_objective_function(theta, Smooth_AllR, Smooth_AllR_RND, b, min_knot, max_knot), theta0, options);
+    theta_hat = fminunc(@(theta) GMM_objective_function(theta, Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot), theta0, options);
 
 end
 
 
 %% Local Function: GMM Objective Function
 
-function J = GMM_objective_function(theta, Smooth_AllR, Smooth_AllR_RND, b, min_knot, max_knot)
+function J = GMM_objective_function(theta, Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot)
 
-    g = GMM_moment_conditions(theta, Smooth_AllR, Smooth_AllR_RND, b, min_knot, max_knot);
+    g = GMM_moment_conditions(theta, Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot);
 
     % Use a GMM type optimization with only the first stage optimization
     W = eye(b + 1);
@@ -33,7 +33,7 @@ end
 
 %% Local Function: GMM Moment Conditions
 
-function g = GMM_moment_conditions(theta, Smooth_AllR, Smooth_AllR_RND, b, min_knot, max_knot)
+function g = GMM_moment_conditions(theta, Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot)
 
     months = Smooth_AllR.Properties.VariableNames;
     T = length(months);
@@ -44,15 +44,22 @@ function g = GMM_moment_conditions(theta, Smooth_AllR, Smooth_AllR_RND, b, min_k
         moment_sum = 0;
 
         for t = 1:T
+
+            current_month_realized_ret = Realized_Return{t, 2};
+
             current_month_y = Smooth_AllR{1, months{t}};
             current_month_RND = Smooth_AllR_RND{1, months{t}};
+
+            current_month_y_filtered = current_month_y(current_month_y <= current_month_realized_ret);
+            filtered_size = length(current_month_y_filtered);
+            current_month_RND_filtered = current_month_RND(1:filtered_size);
 
             g_theta = 0;
 
             for i = 1:(b + 1)
-                B_values = Bspline_basis_function_value(3, b, min_knot, max_knot, i, current_month_y);
+                B_values = Bspline_basis_function_value(3, b, min_knot, max_knot, i, current_month_y_filtered);
                 
-                integral = trapz(current_month_y, B_values .* current_month_RND);            
+                integral = trapz(current_month_y_filtered, B_values .* current_month_RND_filtered);            
                 g_theta = g_theta + theta(i) * integral;
             end
 
--- 03  條件密度積分方法（CDI method）/Procedure__Theta_Estimation_Brief.m
+++ 03  條件密度積分方法（CDI method）/Procedure__Theta_Estimation_Brief.m
@@ -1,10 +1,14 @@
 clear; clc
 Path_MainFolder = 'D:\Google\我的雲端硬碟\學術｜研究與論文\論文著作\CDI Method';
+currentFolder = pwd;
 
 
 %% Load the data
 
-Path_Data = fullfile(Path_MainFolder, 'Code', '02  輸出資料');
+Path_Data_01 = fullfile(Path_MainFolder, 'Code', '01  原始資料處理');
+Realized_Return = readtable(fullfile(Path_Data_01, 'Realized_Return.csv'));
+
+Path_Data_02 = fullfile(Path_MainFolder, 'Code', '02  輸出資料');
 
 Smooth_AllK = [];
 Smooth_AllR = [];
@@ -14,7 +18,7 @@ years_to_merge = 1996:2014;
 
 for year = years_to_merge
     
-    input_filename = fullfile(Path_Data, sprintf('Output_Tables_%d.mat', year));
+    input_filename = fullfile(Path_Data_02, sprintf('Output_Tables_%d.mat', year));
         
     if exist(input_filename, 'file')
         data = load(input_filename);
@@ -31,7 +35,7 @@ end
 %% Define the knots for the B-spline
 
 n = 3;                                                                     % Order of the B-spline (cubic B-spline)
-b = 4;                                                                     % Calculate the number of basis functions (b + 1)
+b = 3;                                                                     % Calculate the number of basis functions (b + 1)
 
 Aggregate_Smooth_AllR = Smooth_AllR.Variables;
 
@@ -39,7 +43,7 @@ Aggregate_Smooth_AllR = Smooth_AllR.Variables;
 min_knot = min(Aggregate_Smooth_AllR);
 
 % Find the maximum realized return within the sample
-max_knot = max(Aggregate_Smooth_AllR);                                     % To be modified
+max_knot = max(Realized_Return.realized_ret);
 
 clear Aggregate_Smooth_AllR
 
@@ -49,36 +53,48 @@ clear Aggregate_Smooth_AllR
 Path_CDI = fullfile(Path_MainFolder, 'Code', '03  條件密度積分方法（CDI method）');
 addpath(Path_CDI);
 
-theta_hat = GMM_theta_estimation(Smooth_AllR, Smooth_AllR_RND, b, min_knot, max_knot);
+theta_hat = GMM_theta_estimation(Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot);
 
 disp('Estimated parameters:');
 disp(theta_hat);
 
 
-%% Plot
+%% Plot: Setting
 
 % Specify the month to plot
 t = 1;
+
 months = Smooth_AllR.Properties.VariableNames;
+
+current_month_realized_ret = Realized_Return{t, 2};
 current_month_y = Smooth_AllR{1, months{t}};
+current_month_y_filtered = current_month_y(current_month_y <= current_month_realized_ret);
+
 current_month = months{t};
 
-min_y = min(current_month_y);
-max_y = max(current_month_y);
+min_y = min(current_month_y_filtered);
+max_y = max(current_month_y_filtered);
 
-y_BS = nan(b + 1, length(current_month_y));
+y_BS = nan(b + 1, length(current_month_y_filtered));
 
 for i = 1:(b + 1)
-    y_BS(i, :) = Bspline_basis_function_value(3, b, min_y, max_y, i, current_month_y);
+    y_BS(i, :) = Bspline_basis_function_value(3, b, min_y, max_y, i, current_month_y_filtered);
 end
 clear i
+
+Path_Output = fullfile(Path_MainFolder, 'Code', '03  輸出圖片');
+
+
+%% Plot: with Cubic B-Spline Curve
 
 % Define Color of Each Line
 color_All = ['b'; 'g'; 'm'; 'c'; 'y'; 'k'; 'r'];
 
+Cubic_BSpline_Basis_Functions_Curve = figure;
+
 % Plot Figure: Each Cubic B-Spline Basis Function
 for i = 1:(b + 1)
-    plot(current_month_y, y_BS(i, :), ...
+    plot(current_month_y_filtered, y_BS(i, :), ...
          'LineStyle', '-', ...
          'LineWidth', 1, ...
          'Color', color_All(i))
@@ -87,15 +103,15 @@ end
 grid on
 
 % Plot Figure: A Cubic B-Spline Curve
-y = sum(transpose(theta_hat) .* y_BS, 1);
-plot(current_month_y, y, ...
+g_function_value = sum(transpose(theta_hat) .* y_BS, 1);
+plot(current_month_y_filtered, g_function_value, ...
      'LineStyle', ':', ...
      'LineWidth', 3, ...
      'Color', 'r')
 hold on
 
 % Add Title
-title(['Cubic B-Spline Basis Functions and Curve for ' num2str(current_month)], ...
+title(['Cubic B-Spline Basis Functions and Curve for ' num2str(current_month) ' (b = ' num2str(b) ')'], ...
       'FontSize', 15, ...
       'FontName', 'Times New Roman', ...
       'FontWeight', 'Bold')
@@ -124,4 +140,62 @@ set(gca, 'LooseInset', get(gca, 'TightInset'))
 
 % Clear Variable
 clear color_All
-clear i 
+clear i
+
+filename = ['Cubic_BSpline_Basis_Functions_Curve (b=' num2str(b) ').png'];
+saveas(Cubic_BSpline_Basis_Functions_Curve, fullfile(Path_Output, filename));
+clear filename
+
+
+%% Plot: without Cubic B-Spline Curve
+
+% Define Color of Each Line
+color_All = ['b'; 'g'; 'm'; 'c'; 'y'; 'k'; 'r'];
+
+Cubic_BSpline_Basis_Functions = figure;
+
+% Plot Figure: Each Cubic B-Spline Basis Function
+for i = 1:(b + 1)
+    plot(current_month_y_filtered, y_BS(i, :), ...
+         'LineStyle', '-', ...
+         'LineWidth', 1, ...
+         'Color', color_All(i))
+    hold on
+end
+grid on
+
+% Add Title
+title(['Cubic B-Spline Basis Functions for ' num2str(current_month) ' (b = ' num2str(b) ')'], ...
+      'FontSize', 15, ...
+      'FontName', 'Times New Roman', ...
+      'FontWeight', 'Bold')
+
+% Legend Setting
+for i = 1:(b + 2)
+    if i < (b + 2)
+        type_legend{i} = ['$B^{' num2str(n) '}_{' num2str(i - 1) '} (y)$'];
+    else
+        type_legend{i} = ['$\sum_{i=0}^{' num2str(b) '} \theta_{i} B^{' num2str(n) '}_{i} (y)$'];
+    end
+end
+h = legend(type_legend);
+clear type_legend
+
+set(h, 'FontSize', 15, ...
+       'FontName', 'Times New Roman', ...
+       'FontWeight', 'Bold', ...
+       'Interpreter', 'Latex', ...
+       'Box', 'Off')
+clear h 
+
+set(gca, 'Layer', 'Top')
+set(gca, 'LooseInset', get(gca, 'TightInset'))
+% set(gcf, 'Position', get(0, 'ScreenSize'))
+
+% Clear Variable
+clear color_All
+clear i
+
+filename = ['Cubic_BSpline_Basis_Functions (b=' num2str(b) ').png'];
+saveas(Cubic_BSpline_Basis_Functions, fullfile(Path_Output, filename));
+clear filename
