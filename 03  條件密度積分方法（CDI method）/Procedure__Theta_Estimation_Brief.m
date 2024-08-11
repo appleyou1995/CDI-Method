9cff2aac7edba8d36753b5bd3c195f96e69ca4a7
Iris Yu <jouping.yu@gmail.com>
2024-08-11 19:35:23 +0800

    Use a for loop to estimate theta (from b=3 to 8)

--- 03  條件密度積分方法（CDI method）/Procedure__Theta_Estimation_Brief.m
+++ 03  條件密度積分方法（CDI method）/Procedure__Theta_Estimation_Brief.m
@@ -1,6 +1,5 @@
 clear; clc
 Path_MainFolder = 'D:\Google\我的雲端硬碟\學術｜研究與論文\論文著作\CDI Method';
-currentFolder = pwd;
 
 
 %% Load the data
@@ -9,7 +8,6 @@ Path_Data_01 = fullfile(Path_MainFolder, 'Code', '01  原始資料處理');
 Realized_Return = readtable(fullfile(Path_Data_01, 'Realized_Return.csv'));
 
 Path_Data_02 = fullfile(Path_MainFolder, 'Code', '02  輸出資料');
-
 Smooth_AllK = [];
 Smooth_AllR = [];
 Smooth_AllR_RND = [];
@@ -31,11 +29,10 @@ for year = years_to_merge
     end
 end
 
+clear input_filename year years_to_merge
+
 
 %% Define the knots for the B-spline
-
-n = 3;                                                                     % Order of the B-spline (cubic B-spline)
-b = 3;                                                                     % Calculate the number of basis functions (b + 1)
 
 Aggregate_Smooth_AllR = Smooth_AllR.Variables;
 
@@ -48,154 +45,19 @@ max_knot = max(Realized_Return.realized_ret);
 clear Aggregate_Smooth_AllR
 
 
-%% Summarize step 3 - step 7 above with one function
+%% Estimate theta
 
-Path_CDI = fullfile(Path_MainFolder, 'Code', '03  條件密度積分方法（CDI method）');
-addpath(Path_CDI);
+Path_Data_03 = fullfile(Path_MainFolder, 'Code', '03  條件密度積分方法（CDI method）');
+addpath(Path_Data_03);
 
-theta_hat = GMM_theta_estimation(Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot);
+Path_Output = fullfile(Path_MainFolder, 'Code', '03  輸出資料');
 
-disp('Estimated parameters:');
-disp(theta_hat);
-
-
-%% Plot: Setting
-
-% Specify the month to plot
-t = 1;
-
-months = Smooth_AllR.Properties.VariableNames;
-
-current_month_realized_ret = Realized_Return{t, 2};
-current_month_y = Smooth_AllR{1, months{t}};
-current_month_y_filtered = current_month_y(current_month_y <= current_month_realized_ret);
-
-current_month = months{t};
-
-min_y = min(current_month_y_filtered);
-max_y = max(current_month_y_filtered);
-
-y_BS = nan(b + 1, length(current_month_y_filtered));
-
-for i = 1:(b + 1)
-    y_BS(i, :) = Bspline_basis_function_value(3, b, min_y, max_y, i, current_month_y_filtered);
+for b = 3:8
+    theta_hat = GMM_theta_estimation(Smooth_AllR, Smooth_AllR_RND, Realized_Return, b, min_knot, max_knot);
+    
+    disp(['b = ' num2str(b) '  Estimated parameters:']);
+    disp(theta_hat);
+    
+    save_filename = ['theta_hat (b=' num2str(b) ').mat'];
+    save(fullfile(Path_Output, save_filename), 'theta_hat');
 end
-clear i
-
-Path_Output = fullfile(Path_MainFolder, 'Code', '03  輸出圖片');
-
-
-%% Plot: with Cubic B-Spline Curve
-
-% Define Color of Each Line
-color_All = ['b'; 'g'; 'm'; 'c'; 'y'; 'k'; 'r'];
-
-Cubic_BSpline_Basis_Functions_Curve = figure;
-
-% Plot Figure: Each Cubic B-Spline Basis Function
-for i = 1:(b + 1)
-    plot(current_month_y_filtered, y_BS(i, :), ...
-         'LineStyle', '-', ...
-         'LineWidth', 1, ...
-         'Color', color_All(i))
-    hold on
-end
-grid on
-
-% Plot Figure: A Cubic B-Spline Curve
-g_function_value = sum(transpose(theta_hat) .* y_BS, 1);
-plot(current_month_y_filtered, g_function_value, ...
-     'LineStyle', ':', ...
-     'LineWidth', 3, ...
-     'Color', 'r')
-hold on
-
-% Add Title
-title(['Cubic B-Spline Basis Functions and Curve for ' num2str(current_month) ' (b = ' num2str(b) ')'], ...
-      'FontSize', 15, ...
-      'FontName', 'Times New Roman', ...
-      'FontWeight', 'Bold')
-
-% Legend Setting
-for i = 1:(b + 2)
-    if i < (b + 2)
-        type_legend{i} = ['$B^{' num2str(n) '}_{' num2str(i - 1) '} (y)$'];
-    else
-        type_legend{i} = ['$\sum_{i=0}^{' num2str(b) '} \theta_{i} B^{' num2str(n) '}_{i} (y)$'];
-    end
-end
-h = legend(type_legend);
-clear type_legend
-
-set(h, 'FontSize', 15, ...
-       'FontName', 'Times New Roman', ...
-       'FontWeight', 'Bold', ...
-       'Interpreter', 'Latex', ...
-       'Box', 'Off')
-clear h 
-
-set(gca, 'Layer', 'Top')
-set(gca, 'LooseInset', get(gca, 'TightInset'))
-% set(gcf, 'Position', get(0, 'ScreenSize'))
-
-% Clear Variable
-clear color_All
-clear i
-
-filename = ['Cubic_BSpline_Basis_Functions_Curve (b=' num2str(b) ').png'];
-saveas(Cubic_BSpline_Basis_Functions_Curve, fullfile(Path_Output, filename));
-clear filename
-
-
-%% Plot: without Cubic B-Spline Curve
-
-% Define Color of Each Line
-color_All = ['b'; 'g'; 'm'; 'c'; 'y'; 'k'; 'r'];
-
-Cubic_BSpline_Basis_Functions = figure;
-
-% Plot Figure: Each Cubic B-Spline Basis Function
-for i = 1:(b + 1)
-    plot(current_month_y_filtered, y_BS(i, :), ...
-         'LineStyle', '-', ...
-         'LineWidth', 1, ...
-         'Color', color_All(i))
-    hold on
-end
-grid on
-
-% Add Title
-title(['Cubic B-Spline Basis Functions for ' num2str(current_month) ' (b = ' num2str(b) ')'], ...
-      'FontSize', 15, ...
-      'FontName', 'Times New Roman', ...
-      'FontWeight', 'Bold')
-
-% Legend Setting
-for i = 1:(b + 2)
-    if i < (b + 2)
-        type_legend{i} = ['$B^{' num2str(n) '}_{' num2str(i - 1) '} (y)$'];
-    else
-        type_legend{i} = ['$\sum_{i=0}^{' num2str(b) '} \theta_{i} B^{' num2str(n) '}_{i} (y)$'];
-    end
-end
-h = legend(type_legend);
-clear type_legend
-
-set(h, 'FontSize', 15, ...
-       'FontName', 'Times New Roman', ...
-       'FontWeight', 'Bold', ...
-       'Interpreter', 'Latex', ...
-       'Box', 'Off')
-clear h 
-
-set(gca, 'Layer', 'Top')
-set(gca, 'LooseInset', get(gca, 'TightInset'))
-% set(gcf, 'Position', get(0, 'ScreenSize'))
-
-% Clear Variable
-clear color_All
-clear i
-
-filename = ['Cubic_BSpline_Basis_Functions (b=' num2str(b) ').png'];
-saveas(Cubic_BSpline_Basis_Functions, fullfile(Path_Output, filename));
-clear filename
