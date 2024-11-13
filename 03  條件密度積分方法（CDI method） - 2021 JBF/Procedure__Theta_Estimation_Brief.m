80f11e3abec9e531543f7dd5748d84f6e82259e9
Iris Yu <jouping.yu@gmail.com>
2024-11-13 23:29:04 +0800

    Estimate theta including nonlinear constraints: g>=0 (ceil)

--- 03  條件密度積分方法（CDI method） - 2021 JBF/Procedure__Theta_Estimation_Brief.m
+++ 03  條件密度積分方法（CDI method） - 2021 JBF/Procedure__Theta_Estimation_Brief.m
@@ -59,6 +59,6 @@ for b = 3:8
     disp(['b = ' num2str(b) '  Estimated parameters:']);
     disp(theta_hat);
     
-    save_filename = ['floor_theta_hat (b=' num2str(b) ').mat'];
+    save_filename = ['ceil_theta_hat (b=' num2str(b) ').mat'];
     save(fullfile(Path_Output, save_filename), 'theta_hat');
 end
